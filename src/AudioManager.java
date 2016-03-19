317dd706c0e3ca5672816a153bc3cf4f03c30bbd
Rane Lafraze <lafrazerl@gmail.com>
2016-03-19 18:10:33 -0400

    Switch the class's purpose from capturin audio to managing it. Make changes to implement the Capture class

--- src/AudioManager.java
+++ src/AudioManager.java
@@ -1,11 +1,44 @@
+import javax.sound.sampled.AudioInputStream;
+
 
 public class AudioManager {
 	
+	AudioInputStream audioInput;
 	
 	public AudioManager() {
 		
 		
 		
+	}
+	
+	/**
+	 * Captures audio for the give amount of time in milliseconds.
+	 * 
+	 * @param timeToCapture
+	 * the time, in milliseconds, to record for
+	 */
+	public void captureAudio(int millis) { 
+		
+		Capture capture = new Capture();
+		capture.start();
+		delay(millis);
+		capture.stop();
+		
+	}
+	
+	/**
+	 * Delays, or waits, the given amount of time in milliseconds.
+	 * 
+	 * @param millis
+	 * the amount of time to wait in milliseconds
+	 */
+	private void delay(int millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 }
